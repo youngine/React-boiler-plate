9730927e8cad58d386f455e21472b47267822cc6
DESKTOP-1EEHAE2\LGUser <youngine012@naver.com>
2021-04-01 11:20:32 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose= require('mongoose')
+mongoose.connect('mongodb+srv://yeongin:7590aa@boilerplate.rpkzh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology : true, useCreateIndex : true, useFindAndModify : false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err=> console.log(err))
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
